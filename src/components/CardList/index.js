d388eb9cf2ced2cd63274337015316692ae4808d
ms <ms@gmail.com>
2020-11-12 22:57:41 +0200

    Add movie db state

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {Card} from "../Card";
 import axios from "axios";
 
@@ -6,6 +6,7 @@ const apiKey = "7fc8d00ab1302c6aa7da237d2810116e"; //temporary
 const baseUrl = "https://api.themoviedb.org/3/search/movie";
 
 export const CardList = () => {
+    const [movieData, setMovieData] = useState(null);
     useEffect(() =>{
         axios.get(baseUrl,{
             params:{
@@ -14,10 +15,10 @@ export const CardList = () => {
                 query:'Star Wars' //TODO: from input
             }
         })
-        .then((response) => console.log(response))
+        .then((res) => setMovieData(res.data))
         .catch((err) => console.log(err))
     },[])
-
+console.log(movieData);
     return (
         <Card/>
     )
