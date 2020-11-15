525495efa51b0a5264683dbe73fe1d4b3d55586a
ms <ms@gmail.com>
2020-11-15 20:51:23 +0200

    Add Link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Movie from "./Movie"
 import MovieDetails from "./MovieDetails"
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar"
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Navbar/>
             <Switch>
                 <Route path="/detail/:id" component={MovieDetails} exact/>
                 <Route path="/" component={Movie}/>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div>
-            
+           <Link to="/"> HOME </Link> 
         </div>
     )
 }
