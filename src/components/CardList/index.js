bb3a5b9be23358043c282da98ef4e87996b85d46
ferruhcihan <ferruhcihan20@gmail.com>
2020-11-15 18:09:53 +0300

    fix noPoster bug

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -10,7 +10,7 @@ export const CardList = ({movieList, baseImageUrl}) => {
                <Card 
                 key={index} 
                 title={movie.title}
-                imgSrc={baseImageUrl + movie.poster_path}
+                imgSrc={movie.poster_path ? baseImageUrl + movie.poster_path : "https://images.unsplash.com/photo-1518676590629-3dcbd9c5a5c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"}
                 />)
             )}
         </StyledCardList>
