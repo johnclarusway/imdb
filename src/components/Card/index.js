5bc415daa9258c1b5685fb8edd685cc3b359acff
mrtarikozturk <55311492+mrtarikozturk@users.noreply.github.com>
2020-11-16 16:01:36 +0100

    Update index.js
    Add "No Poster" image

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,15 @@
 import { useHistory } from "react-router-dom";
 import {StyledCardWrapper, StyledText, StyledMovieImage} from "./Card.style"
 
-export const Card = ({title, imgSrc, id}) => {
+export const Card = ({title, imgSrc, id, poster_path}) => {
     const history = useHistory();
     return (
         <StyledCardWrapper onClick={() => history.push(`/detail/${id}`)}>
-            <StyledMovieImage src={imgSrc} alt={"Movie Poster"}/>
+        {
+            poster_path ?
+            <StyledMovieImage src={imgSrc} alt={"Movie Poster"} /> :
+            <StyledMovieImage src='https://via.placeholder.com/113x170.png?text=No+Image' alt={"No Poster"} />
+        }
             <StyledText>{title}</StyledText>
         </StyledCardWrapper>
     )
